c5fd29e37eca6000811ad099ff214062e0973c46
ayoam <you@example.com>
2023-08-30 12:47:15 +0100

    updated swagger config

--- main.go
+++ main.go
@@ -16,13 +16,6 @@ import (
 
 // @title Go Fiber Rest API
 // @version 1.0
-// @description This is a sample swagger for Fiber
-// @termsOfService http://swagger.io/terms/
-// @contact.name API Support
-// @contact.email fiber@swagger.io
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-// @host localhost:8000
 // @BasePath /api/v1/
 func main() {
 
